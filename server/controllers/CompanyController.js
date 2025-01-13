ac13adbafb4f957b7840cf2a906e3cafba067959
Rushikesh1 Avachat <avachatrushikesh45@gmail.com>
2025-01-13 12:03:15 +0530

    env

---
+++ server/controllers/CompanyController.js
@@ -0,0 +1,24 @@
+export const registerCompany=async(req, res)=>{
+
+
+}
+
+export const loginCompany=async(req, res)=>{
+
+}
+
+export const getComoanyData=async(req, res)=>{
+
+}
+export const postJob=async(req,res)=>{
+
+}
+export const getCompanyJobApplicants=async(req,res)=>{
+
+}
+export const changeJobApplicationStatus=async(req,res)=>{
+
+}
+export const changeVisibility=async(req,res)=>{
+
+}
